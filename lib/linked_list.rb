10bbb15b2e6913b8ba83b8e83cf8e6bd6beaa9e0
Dane Hale <danehale0612@gmail.com>
2013-04-22 15:11:12 -0500

    finished exercises and refactored code based on notes

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,18 +7,60 @@ class LinkedList
   end
 
   def add_item(payload)
+    
+    item = LinkedListItem.new(payload)
+    if @first_item.nil?
+      @first_item = item
+    else
+      final_item = @first_item
+      while !final_item.last?
+        final_item = final_item.next_list_item
+      end
+      final_item.next_list_item = item
+    end
   end
 
   def get(n)
+    item = @first_item
+    n.times do
+      raise IndexError if item.nil?
+      item = item.next_list_item
+    end
+    item.payload
   end
 
   def last
+    item = @first_item
+    return nil if item.nil?
+    until item.last?
+      item = item.next_list_item
+    end
+    item.payload
   end
 
+  
   def size
+    size = 0
+    index = @first_item
+    until index.nil? do 
+      index = index.next_list_item
+      size += 1
+    end
+    size
   end
 
   def to_s
+    if @first_item.nil?
+      "| |"
+    else
+      item = @first_item
+      payloads = item.payload
+      while !item.last?
+        item = item.next_list_item
+        payloads += ", " + item.payload
+      end
+     return  "| #{payloads} |"
+    end
   end
 
   # ========= Bonus ========== #
